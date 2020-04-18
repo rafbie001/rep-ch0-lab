87cd818c67fbf85a0daf13f56acced6331f328ea
Łukasz Kręski <lukaszcezarykreski@gmail.com>
2020-04-18 13:06:31 +0200

    Inicjalizacja

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Wersja JDK: "+System.getProperty("java.version"));
+        System.out.println("Wersja JRE: "System.getProperty("java.runtime.version"));
+    }
+}
