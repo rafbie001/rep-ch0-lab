2764cd825b0db526bc1ee536f302f59f165d62c3
rafbie001 <rafal.biernacki@windowslive.com>
2020-04-18 14:01:57 +0200

    dodanie nazwy proj

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+
+        String userDir = System.getProperty("user.dir");
+        Path path = Paths.get(userDir);
+        String project = path.getFileName();
+
         System.out.println("Wersja JDK: "+System.getProperty("java.version"));
         System.out.println("Wersja JRE: "System.getProperty("java.runtime.version"));
+        System.out.println(project));
     }
 }
